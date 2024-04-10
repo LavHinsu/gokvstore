0cbe2256c1545dff478fb33175ffbbd9c248236c
Lav Hinsu <hinsulav105@gmail.com>
2024-04-11 00:11:52 +0530

    basic http server

--- cmd/gokvstore/main.go
+++ cmd/gokvstore/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "200 ok")
+}
 
 func main() {
-	fmt.Println("hello world from the file structure")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
