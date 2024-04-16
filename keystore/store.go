6132a30d827f844aeae4625dada744d5228a1481
Lav Hinsu <hinsulav105@gmail.com>
2024-04-17 01:04:51 +0530

    skeleton for main store

--- keystore/store.go
+++ keystore/store.go
@@ -1,7 +1,30 @@
 package keystore
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func Keystore() {
-	fmt.Println("Inside keystore")
+var (
+	keyStoreMap = make(map[string]string)
+)
+
+func init() {
+	keyStoreMap["foo"] = "bar"
+	keyStoreMap["good"] = "stuff"
+}
+
+func Getkey(key string) string {
+	log.Print("getting key", key)
+	value, ok := keyStoreMap[key]
+	if ok {
+		return value
+	} else {
+		log.Printf("key not found")
+		return "key not found"
+	}
+}
+
+func Addkey() {
+	fmt.Println("Inside addkey")
 }
--- server/gokvstore/main.go
+++ server/gokvstore/main.go
@@ -2,25 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/LavHinsu/gokvstore/keystore"
 )
 
 func getKey(w http.ResponseWriter, req *http.Request) {
-	idString := req.PathValue("id")
-	keystore.Keystore()
-	fmt.Fprint(w, "Got ID ", idString)
+	key := req.PathValue("id")
+	value := keystore.Getkey(key)
+	fmt.Fprint(w, value)
 }
 
 func postKey(w http.ResponseWriter, req *http.Request) {
-	// idString := req.PathValue("id") todo: add mechanism to extract data from post req and put in memory
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Printf("Error reading body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
+		return
+	} else {
+		log.Print(string(body))
+	}
+	keystore.Addkey()
+	fmt.Fprint(w, "200 ok")
+}
+
+func healthCheck(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "200 ok")
 }
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthcheck", healthCheck) // healthcheck for a probe if this is run in kubernetes
 	mux.HandleFunc("GET /key/{id}", getKey)
-	mux.HandleFunc("POST /writekey/{id}", postKey)
+	mux.HandleFunc("POST /writekey", postKey)
+
 	http.ListenAndServe("localhost:8080", mux)
 }
