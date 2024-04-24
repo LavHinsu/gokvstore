50c36d0c3ca59bde38de6abc34e61996f5f3428d
Lav Hinsu <hinsulav105@gmail.com>
2024-04-25 02:20:04 +0530

    add readlock and update performance report

--- keystore/getkey.go
+++ keystore/getkey.go
@@ -4,6 +4,8 @@ import "log"
 
 // function used to get a key from our store
 func Getkey(key string, ipaddr string) *keyStoreMap {
+	mutex.RLock()
+	defer mutex.RUnlock()
 	value, ok := keyStore[key]
 	if ok {
 		log.Println(ipaddr + " get key: " + key)
