eed891300904be2f38e088ab78e641e69c43d255
Lav Hinsu <hinsulav105@gmail.com>
2024-04-25 01:14:08 +0530

    update read-func to not lock writes as read is routine safe

--- keystore/store.go
+++ keystore/store.go
@@ -37,8 +37,6 @@ func init() {
 
 // function used to get a key from our store
 func Getkey(key string, ipaddr string) *keyStoreMap {
-	mutex.RLock()
-	defer mutex.RUnlock()
 	value, ok := keyStore[key]
 	if ok {
 		log.Println(ipaddr + " get key: " + key)
